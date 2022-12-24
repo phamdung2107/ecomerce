4bfe59eb73781c827e8ed5fb8717c7198b76b166
phamdung2107 <phamdung21072002@gmail.com>
2022-12-24 16:22:30 +0700

    push

---
+++ src/components/user/UpdatePassword.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import "./UpdatePassword.css";
+import Loading from "../../more/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import MetaData from "../../more/Metadata";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import LockIcon from "@mui/icons-material/Lock";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import { clearErrors, updatePassword } from "../../actions/userAction";
+import { UPDATE_PASSWORD_RESET } from "../../constans/userContans";
+import { useNavigate } from "react-router-dom";
+
+const UpdatePassword = () => {
+
+const dispatch = useDispatch();
+
+const history = useNavigate();
+
+  const { error, isUpdated, loading } = useSelector((state) => state.profile);
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const updatePasswordSubmit = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set("oldPassword", oldPassword);
+    myForm.set("newPassword", newPassword);
+    myForm.set("confirmPassword", confirmPassword);
+
+    dispatch(updatePassword(myForm));
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert(error);
+      dispatch(clearErrors());
+    }
+
+    if (isUpdated) {
+      alert("Profile Updated Successfully");
+      history("/me");
+      dispatch({
+        type: UPDATE_PASSWORD_RESET,
+      });
+    }
+  }, [dispatch, error, history, isUpdated]);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <MetaData title="Change Password" />
+          <div className="updatePasswordContainer">
+            <div className="updatePasswordBox">
+              <h2 className="updatePasswordHeading">Update Profile</h2>
+
+              <form
+                className="updatePasswordForm"
+                onSubmit={updatePasswordSubmit}
+              >
+                <div className="loginPassword">
+                  <VpnKeyIcon />
+                  <input
+                    type="password"
+                    placeholder="Old Password"
+                    required
+                    value={oldPassword}
+                    onChange={(e) => setOldPassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="loginPassword">
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="New Password"
+                    required
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </div>
+                <div className="loginPassword">
+                  <LockIcon />
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    required
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Change"
+                  className="updatePasswordBtn"
+                />
+              </form>
+            </div>
+          </div>
+        </>
+      )}
+      {/* <BottomTab /> */}
+    </>
+  );
+};
+
+export default UpdatePassword
